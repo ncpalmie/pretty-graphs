5592a7b38886ccb26fa60e4c2c016557e814d55e
ncpalmie <palmieronathan@gmail.com>
2021-07-15 20:53:19 -0400

    Setting up initial structure with Graph as the main parent component

---
+++ src/Graph.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import './css/Graph.css';
+
+function Graph() {
+  const [viewX, setViewX] = useState(0);
+  return (
+    <div className="Graph-Container">
+      
+    </div>
+  );
+}
+
+export default Graph;
